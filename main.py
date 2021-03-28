2e9d39c504675f1abb02fa916921995a9c1b4c58
alexthejpeg <alexphan85120@gmail.com>
2021-03-28 01:55:03 -0400

    Add comments and docstrings

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
-        self.stateSelect.currentTextChanged.connect(self.calculate_required_payments)
+        # Dropdowns, number / text inputs, and buttons
+        self.stateSelect.currentTextChanged.connect(self.calculate_required_monthly_payments)
 
-        self.groceriesBox.valueChanged.connect(self.update_total_payments)
-        self.housingBox.valueChanged.connect(self.update_total_payments)
-        self.utilitiesBox.valueChanged.connect(self.update_total_payments)
-        self.transportationBox.valueChanged.connect(self.update_total_payments)
-        self.miscBox.valueChanged.connect(self.update_total_payments)
+        self.groceriesBox.valueChanged.connect(self.update_total_payments_box)
+        self.housingBox.valueChanged.connect(self.update_total_payments_box)
+        self.utilitiesBox.valueChanged.connect(self.update_total_payments_box)
+        self.transportationBox.valueChanged.connect(self.update_total_payments_box)
+        self.miscBox.valueChanged.connect(self.update_total_payments_box)
 
         self.addExpense.clicked.connect(self.add_expense)
         self.removeExpenses.clicked.connect(self.remove_expenses)
@@ -27,6 +28,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.addAsset.clicked.connect(self.add_asset)
         self.removeAssets.clicked.connect(self.remove_assets)
 
+        self.incomeBox.valueChanged.connect(self.update_balance)
+
+        self.percentageLabel.setHidden(True)
+        self.flatOrPercentage.currentTextChanged.connect(self.update_flat_percentage)
+
+        self.addAllotment.clicked.connect(self.add_allotment)
+        self.removeAllotment.clicked.connect(self.remove_allotments)
+
+        # Variables
         self.expenses = []
         self.total_expenses = 0
         self.grand_total_expenses = 0
@@ -34,19 +44,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.assets = []
         self.grand_total_assets = 0
 
+        self.allotments = []
+
         self.income = 0
         self.balance = 0
 
-        self.incomeBox.valueChanged.connect(self.update_balance)
-
-        self.percentageLabel.setHidden(True)
-        self.flatOrPercentage.currentTextChanged.connect(self.update_flat_percentage)
-
-        self.allotments = []
-
-        self.addAllotment.clicked.connect(self.add_allotment)
-        self.removeAllotment.clicked.connect(self.remove_allotments)
-
+        # Themes
         self.light_theme = self.styleSheet()
         self.dark_theme = qdarkstyle.load_stylesheet()
 
@@ -58,7 +61,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowTitle("Phynance Manager")
         self.show()
 
-    def calculate_required_payments(self, state):
+    def calculate_required_monthly_payments(self, state):
+        """Calculates the required monthly payments for a state.
+
+        Args:
+            state (str): The state selected in the dropdown.
+        """
         if state != "Select a state...":
             grocery_cost = round(coli.get_state_cost(state, "grocery") / 12, 2)
             housing_cost = round(coli.get_state_cost(state, "housing") / 12, 2)
@@ -72,10 +80,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.transportationBox.setValue(transportation_cost)
             self.miscBox.setValue(misc_cost)
 
-            self.update_total_payments()
+            self.update_total_payments_box()
             self.update_expenses()
 
-    def update_total_payments(self):
+    def update_total_payments_box(self):
+        """Sums up the costs in each category and updates the total monthly payments box."""
         grocery_cost = self.groceriesBox.value()
         housing_cost = self.housingBox.value()
         utilities_cost = self.utilitiesBox.value()
@@ -86,6 +95,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.totalBox.setText("{:.2f}".format(self.total_expenses))
 
     def add_expense(self):
+        """Add an expense given the date, description, recipient, and amount."""
         self.expenses.append(
             {
                 "date": self.expenseCalendar.selectedDate().toString("M/d/yyyy"),
@@ -97,6 +107,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_expenses()
 
     def remove_expenses(self):
+        """Remove a selection of expenses from the list."""
         selections = self.expensesTable.selectedItems()
         rows_to_delete = reversed(
             sorted(list({selection.row() for selection in selections}))
@@ -108,6 +119,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_expenses()
 
     def update_expenses(self):
+        """Update the expenses table to correctly show the expenses in the order they were added."""
         self.expensesTable.clearContents()
         self.expensesTable.setRowCount(len(self.expenses))
 
@@ -126,6 +138,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_balance()
 
     def add_asset(self):
+        """Add an asset given the date, description, and value."""
         self.assets.append(
             {
                 "date": self.assetCalendar.selectedDate().toString("M/d/yyyy"),
@@ -136,6 +149,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_assets()
 
     def remove_assets(self):
+        """Remove a selection of assets from the list."""
         selections = self.assetsTable.selectedItems()
         rows_to_delete = reversed(
             sorted(list({selection.row() for selection in selections}))
@@ -147,6 +161,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_assets()
 
     def update_assets(self):
+        """Update the assets table to correctly show the assets in the order they were added."""
         self.assetsTable.clearContents()
         self.assetsTable.setRowCount(len(self.assets))
 
@@ -164,6 +179,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_balance()
 
     def update_balance(self):
+        """Update the seven-segment display for the balance."""
         self.income = self.incomeBox.value()
         self.balance = round(self.income + self.grand_total_assets - self.grand_total_expenses, 2)
         self.balanceDisplay.display(self.balance)
@@ -171,10 +187,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_allotments()
 
     def update_flat_percentage(self, selection):
+        """Hide the dollar sign / percent label depending on the selection.
+
+        Args:
+            selection (str): Either "Flat" or "Percentage".
+        """
         self.flatLabel.setHidden(selection == "Percentage")
         self.percentageLabel.setHidden(selection == "Flat")
 
     def add_allotment(self):
+        """Add an allotment given the category, priority, amount, and flat / percentage."""
         self.allotments.append(
             {
                 "category": self.categoryBox.text(),
@@ -188,6 +210,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_allotments()
 
     def remove_allotments(self):
+        """Remove a selection of allotments from the list."""
         selections = self.allotmentsTable.selectedItems()
         rows_to_delete = reversed(
             sorted(list({selection.row() for selection in selections}))
@@ -199,6 +222,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.update_allotments()
 
     def update_allotments(self):
+        """Update the allotments table to correctly show the allotments in priority order. 1 is the highest priority."""
         self.allotmentsTable.clearContents()
         self.allotmentsTable.setRowCount(len(self.allotments))
 
@@ -228,9 +252,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.allotmentsTable.setItem(index, 4, QTableWidgetItem("${:.2f}".format(allotment["remainder"])))
 
     def change_to_light_theme(self):
+        """Change to the default theme."""
         self.setStyleSheet(self.light_theme)
 
     def change_to_dark_theme(self):
+        """Change to qdarkstyle's dark theme."""
         self.setStyleSheet(self.dark_theme)
 
 
